a5c84bc81798c5426136ceadf2e134ec1bb5b72f
alkkas <alliscakess@gmail.com>
2024-05-25 11:18:03 +0300

    add start lexser

--- src/app/model/lex/lex.model.ts
+++ src/app/model/lex/lex.model.ts
@@ -1,6 +1,6 @@
 type LexModel = {
   keywords: string[]
   operators: string[]
-  comments: { singleLine: RegExp; multiLine: { start: RegExp; finish: RegExp } }
-  tokens: Record<string, unknown>
+  // comments: { singleLine: RegExp; multiLine: { start: RegExp; finish: RegExp } }
+  tokenizer: 
 }
