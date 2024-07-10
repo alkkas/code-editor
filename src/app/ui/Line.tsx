f1d10f0242189951c1d0b009c3a884e4fd9ca797
Черных Егор Кириллович <e.chernykh@dpractices.local>
2024-07-10 16:55:02 +0300

    refactor line and symbol

--- src/app/ui/Line.tsx
+++ src/app/ui/Line.tsx
@@ -2,102 +2,54 @@ import LineNumber from '@/shared/ui/LineNumber/LineNumber'
 import { ISymbol } from '@/store/editorStore.types'
 import { memo } from 'react'
 import { useEditorStore } from '@/store/editorStore'
-import { lineAttr, symbolAttr } from '@/shared/utils/lib/elements.const'
+import { lineAttr } from '@/shared/utils/lib/elements.const'
+import Symbol from './Symbol'
 
 interface LineProps {
   line: ISymbol[]
   index: number
 }
 
-const areLinesEqual = (line1: ISymbol[], line2: ISymbol[]) => {
-  if (line1.length !== line2.length) return false
-
-  for (let i = 0; i < line1.length; i++) {
-    if (
-      line1[i].value !== line2[i].value ||
-      line1[i].color !== line2[i].color
-    ) {
-      return false
-    }
+const areLineInRange = (lineProps: LineProps) => {
+  const editorStore = useEditorStore()
+  const range = editorStore.getSelectionRange()
+  if (
+    lineProps.index >= range.start.lineIndex &&
+    lineProps.index <= range.finish.lineIndex
+  ) {
+    return true
   }
+}
+
+const areLinesEqual = (linePrev: LineProps, lineCurr: LineProps) => {
+  if (linePrev.index !== lineCurr.index) return false
+
+  if (linePrev.line.length !== lineCurr.line.length) return false
+
+  if (areLineInRange(linePrev) !== areLineInRange(lineCurr)) return false
+
   return true
 }
 
-const Line = memo(
-  function Line(props: LineProps) {
-    const editorStore = useEditorStore()
-
-    const symbolBorder = (lineIndex: number, symbolIndex: number) => {
-      let symbolClassName = ''
-
-      if (!editorStore.isSelectionActive()) return symbolClassName
-
-      const { start, finish } = editorStore.getSelectionRange()
-      //TODO: fix bugs
-      if (
-        lineIndex >= start.lineIndex &&
-        lineIndex <= finish.lineIndex &&
-        (lineIndex === start.lineIndex
-          ? symbolIndex >= start.indexInLine
-          : true) &&
-        (lineIndex === finish.lineIndex
-          ? symbolIndex <= finish.indexInLine
-          : true)
-      ) {
-        symbolClassName += 'bg-slate-400'
-
-        if (
-          (lineIndex === start.lineIndex &&
-            symbolIndex === start.indexInLine) ||
-          symbolIndex === 0
-        ) {
-          symbolClassName += ' rounded-l-md'
-        }
-
-        if (
-          (lineIndex === finish.lineIndex &&
-            symbolIndex === finish.indexInLine) ||
-          symbolIndex === editorStore.lines[lineIndex].length - 1
-        ) {
-          symbolClassName += ' rounded-r-md'
-        }
-      }
-
-      return symbolClassName
-    }
-
-    return (
-      <div key={props.index} className="flex items-stretch">
-        <LineNumber count={props.index + 1} />
-        <div
-          {...{ [lineAttr]: props.index }}
-          className="flex items-center w-full"
-        >
-          {props.line.map((symbol, symbol_idx) => (
-            <span
-              key={symbol_idx}
-              {...{ [symbolAttr]: symbol_idx }}
-              style={{ color: symbol.color ?? 'black' }}
-              className={symbolBorder(props.index, symbol_idx)}
-            >
-              {symbol.value === ' ' ? <>&nbsp;</> : symbol.value}
-            </span>
-          ))}
-        </div>
+const Line = (props: LineProps) => {
+  return (
+    <div key={props.index} className="flex items-stretch">
+      <LineNumber count={props.index + 1} />
+      <div
+        {...{ [lineAttr]: props.index }}
+        className="flex items-center w-full"
+      >
+        {props.line.map((symbol, symbolIdx) => (
+          <Symbol
+            lineIdx={props.index}
+            symbol={symbol}
+            symbolIdx={symbolIdx}
+            key={symbolIdx + symbol.value}
+          />
+        ))}
       </div>
-    )
-  },
-  (prevProps, nextProps) => {
-    //TODO: fix memoization
-    return false
-    const editorStore = useEditorStore.getState()
+    </div>
+  )
+}
 
-    if (prevProps.index !== nextProps.index) return false
-
-    return (
-      editorStore.currentCarriagePos.lineIndex !== nextProps.index &&
-      areLinesEqual(prevProps.line, nextProps.line)
-    )
-  }
-)
-export default Line
+export default memo(Line, areLinesEqual)
--- src/app/ui/Symbol.tsx
+++ src/app/ui/Symbol.tsx
@@ -1,17 +1,64 @@
-interface SymolProps {
-  symbol_idx: number
-  attrs: Record<string, any>
+import { symbolAttr } from '@/shared/utils/lib/elements.const'
+import { useEditorStore } from '@/store/editorStore'
+import { ISymbol } from '@/store/editorStore.types'
+
+interface SymbolProps {
+  symbolIdx: number
+  lineIdx: number
+  symbol: ISymbol
 }
 
-export const Symbol = (props: SymbolProps) => {
+const Symbol = (props: SymbolProps) => {
+  const editorStore = useEditorStore()
+
+  const symbolBorder = (lineIndex: number, symbolIndex: number) => {
+    let symbolClassName = ''
+
+    if (!editorStore.isSelectionActive()) return symbolClassName
+
+    const { start, finish } = editorStore.getSelectionRange()
+    //TODO: fix bugs
+    if (
+      lineIndex >= start.lineIndex &&
+      lineIndex <= finish.lineIndex &&
+      (lineIndex === start.lineIndex
+        ? symbolIndex >= start.indexInLine
+        : true) &&
+      (lineIndex === finish.lineIndex
+        ? symbolIndex <= finish.indexInLine
+        : true)
+    ) {
+      symbolClassName += 'bg-slate-400'
+
+      if (
+        (lineIndex === start.lineIndex && symbolIndex === start.indexInLine) ||
+        symbolIndex === 0
+      ) {
+        symbolClassName += ' rounded-l-md'
+      }
+
+      if (
+        (lineIndex === finish.lineIndex &&
+          symbolIndex === finish.indexInLine) ||
+        symbolIndex === editorStore.lines[lineIndex].length - 1
+      ) {
+        symbolClassName += ' rounded-r-md'
+      }
+    }
+
+    return symbolClassName
+  }
+
   return (
     <span
-      key={symbol_idx}
-      {...{ [symbolAttr]: symbol_idx }}
-      style={{ color: symbol.color ?? 'black' }}
-      className={symbolBorder(props.index, symbol_idx)}
+      key={props.symbolIdx}
+      {...{ [symbolAttr]: props.symbolIdx }}
+      style={{ color: props.symbol.color ?? 'black' }}
+      className={symbolBorder(props.lineIdx, props.symbolIdx)}
     >
-      {symbol.value === ' ' ? <>&nbsp;</> : symbol.value}
+      {props.symbol.value === ' ' ? <>&nbsp;</> : props.symbol.value}
     </span>
   )
 }
+
+export default Symbol
